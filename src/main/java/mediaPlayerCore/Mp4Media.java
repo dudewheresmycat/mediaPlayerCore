685ccc7aa53396737abd4a6b4ac81901fa95ffef
dudewheresmycat <dudewheresmycat@users.noreply.github.com>
2015-12-13 03:44:03 -0500

    added classes

---
+++ src/main/java/mediaPlayerCore/Mp4Media.java
@@ -0,0 +1,10 @@
+package mediaPlayerCore;
+
+public class Mp4Media extends MediaFile {
+
+	public Mp4Media(String name, double length, String genre, String artist, String filename) {
+		super(name, length, genre, artist, filename);
+		// TODO Auto-generated constructor stub
+	}
+
+}
